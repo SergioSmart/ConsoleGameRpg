273075d13107b4a627855f00c5e93140280ea679
SergioSmart <serejja2018@gmail.com>
2023-01-15 17:41:55 +0200

    X

--- ConsoleGameRpg/Menus/Intro.cs
+++ ConsoleGameRpg/Menus/Intro.cs
@@ -13,7 +13,9 @@ namespace ConsoleGameRpg.Menus
         public void Print()
         {
             string[] file = File.ReadAllLines("intro.txt");
-
+            Console.Beep();
+            Thread.Sleep(1000);
+            Console.Beep(38, 3);
 
             for (int i = 0; i < file.Length; i++)
             {
