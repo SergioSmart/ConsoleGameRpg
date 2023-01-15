be7213ef4f44e0af2ba4722f547d64ece64b6eed
SergioSmart <serejja2018@gmail.com>
2023-01-15 17:15:28 +0200

    Created Intro

---
+++ ConsoleGameRpg/Menus/Intro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGameRpg.Menus
+{
+    internal class Intro
+    {
+        public Intro() { }
+        
+        public void Print()
+        {
+            File.ReadAllText("intro.txt");
+        }
+
+    }
+}
