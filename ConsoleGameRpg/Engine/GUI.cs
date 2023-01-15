7a792f92c0b40e655a5ff62959c5e250088d1f15
SergioSmart <serejja2018@gmail.com>
2023-01-15 22:46:26 +0200

    X

--- ConsoleGameRpg/Engine/GUI.cs
+++ ConsoleGameRpg/Engine/GUI.cs
@@ -10,16 +10,31 @@ namespace ConsoleGameRpg.Engine
     {
         public GUI() { }
 
-        public void PrintMap(string path)
+        public string[] ReadMap(string path)
         {
             string[] mapFile = File.ReadAllLines(path);
-
-            for (int i = 0; i < mapFile.Length; i++)
-            {
-                Thread.Sleep(15);
-                Console.WriteLine(mapFile[i]);
-            }
+            return mapFile;
         }
 
+        public void WriteIntro(string[] map)
+        {          
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            Console.SetWindowSize(190, 55);
+
+            for (int i = 0; i < map.Length; i++)
+            {
+                Console.SetCursorPosition(48, 9 + i);
+                Thread.Sleep(30);
+                Console.WriteLine(map[i]);    
+            }
+
+            Console.SetCursorPosition(80, 35);
+            
+            Console.WriteLine("Нажмите любую клавишу, что-бы продолжить..."); 
+
+            Console.ReadKey();
+        }
     }
 }
--- ConsoleGameRpg/Engine/MusicBeep.cs
+++ ConsoleGameRpg/Engine/MusicBeep.cs
@@ -14,45 +14,48 @@ namespace ConsoleGameRpg.Engine
         private static byte _step = 0;
         private static byte _cycle = 0;
 
-        public static void PlayTrack1()
+        public static void PlayTrack1(bool isActive)
         {
-            if(_cycle >= 0 && _cycle < 4)
+            while(isActive)
             {
-                while (_length < 4)
+                if (_cycle >= 0 && _cycle < 4)
                 {
-                    Console.Beep(_frequency + _step, _duration);
-                    _step += 17;
-                    _length++;                  
+                    while (_length < 4)
+                    {
+                        Console.Beep(_frequency + _step, _duration);
+                        _step += 17;
+                        _length++;
+                    }
+                    while (_length > 0)
+                    {
+                        Console.Beep(_frequency + _step, _duration);
+                        _step -= 17;
+                        _length--;
+                    }
+                    _cycle++;
                 }
-                while (_length > 0)
+
+                if (_cycle >= 4 && _cycle < 6)
                 {
-                    Console.Beep(_frequency + _step, _duration);
-                    _step -= 17;
-                    _length--;
+                    while (_length < 4)
+                    {
+                        Console.Beep(_frequency + 90 + _step, _duration);
+                        _step += 17;
+                        _length++;
+                    }
+                    while (_length > 0)
+                    {
+                        Console.Beep(_frequency + 90 + _step, _duration);
+                        _step -= 17;
+                        _length--;
+                    }
+                    _cycle++;
                 }
-                _cycle++;
-            }                            
-            
-            if(_cycle >= 4 && _cycle < 6)
-            {
-                while (_length < 4)
+
+                if (_cycle >= 6)
                 {
-                    Console.Beep(_frequency + 90 + _step, _duration);
-                    _step += 17;
-                    _length++;
+                    _cycle = 0;
                 }
-                while (_length > 0)
-                {
-                    Console.Beep(_frequency + 90 + _step, _duration);
-                    _step -= 17;
-                    _length--;                  
-                }
-                _cycle++;
-            }
-            
-            if(_cycle >= 6)
-            {
-                _cycle = 0;
             }
         }
     }
