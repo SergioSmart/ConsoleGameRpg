dc97d03f96c130473dab348fa5ee43ed52b014c2
SergioSmart <serejja2018@gmail.com>
2023-01-15 22:17:43 +0200

    X

--- ConsoleGameRpg/Engine/GUI.cs
+++ ConsoleGameRpg/Engine/GUI.cs
@@ -13,18 +13,6 @@ namespace ConsoleGameRpg.Engine
         public void PrintMap(string path)
         {
             string[] mapFile = File.ReadAllLines(path);
-            //Console.Beep(400, 1000);
-            //Console.Beep(210, 1000);
-            //Console.Beep(220, 1000);
-            //Console.Beep(230, 1000);
-            //Console.Beep(240, 1000);
-            //Console.Beep(250, 1000);
-            //Console.Beep(260, 1000);
-            //Console.Beep(270, 1000);
-            //Console.Beep(280, 1000);
-            //Console.Beep(290, 1000);
-            //Console.Beep(300, 1000);
-            //Console.Beep(1100, 1000);
 
             for (int i = 0; i < mapFile.Length; i++)
             {
