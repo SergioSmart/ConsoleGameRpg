1244956e58c0a5a27792fc5654f94e946a245731
SergioSmart <serejja2018@gmail.com>
2023-01-15 16:20:43 +0200

    Dev 2

---
+++ ConsoleGameRpg/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGameRpg
+{
+    internal class Class1
+    {
+
+        //DEVELOP CLASS 1
+    }
+}
